dd3b1c31240945d068cd0611cb7f748e3d53b104
Alexia Gomes <alexiagommmes@gmail.com>
2017-05-10 14:09:28 -0300

    Implementação do método procurailha

--- src/MatrizesArquivos/MatrizesArquivos.java
+++ src/MatrizesArquivos/MatrizesArquivos.java
@@ -11,21 +11,39 @@ package MatrizesArquivos;
  */
 public class MatrizesArquivos {
 
-    public int contador(int[][] matriz) {
+    public int contador(int[][] matrizIlhas) {
 
-        int matrizLinha = matriz[0].length;
-        int matrizColuna = matriz.length;
+        int verificaLinha = matrizIlhas[0].length;
+        int verificaColuna = matrizIlhas.length;
         int indiceIlhas = 0;
-        boolean[][] verificaIlhas = new boolean[matrizLinha][matrizColuna];
+        boolean[][] verificaIlhas = new boolean[verificaLinha][verificaColuna];
 
-        for (int l = 0; l < matrizLinha; l++) {
-            for (int c = 0; c < matrizColuna; c++) {
-                if (!verificaIlhas[l][c] && matriz[l][c] == 1) {
+        for (int l = 0; l < verificaLinha; l++) {
+            for (int c = 0; c < verificaColuna; c++) {
+                if (!verificaIlhas[l][c] && matrizIlhas[l][c] == 1) {
                     indiceIlhas++;
                 }
             }
         }//fecja for
 
         return 0;
+    }//fecha contador
+
+    public void procuraIlha(int[][] matrizIlhas, boolean[][] verificaIlhas, int verificaLinha, int verificaColuna) {
+
+        if (verificaLinha < 0 || verificaColuna < 0) {
+        }
+
+        if (verificaLinha >= matrizIlhas.length || verificaColuna >= matrizIlhas[0].length) {
+        }
+
+        if (verificaIlhas[verificaLinha][verificaColuna] == true) {
+            verificaIlhas[verificaLinha][verificaColuna] = true;
+        }
+
+        if (matrizIlhas[verificaLinha][verificaColuna] == 0) {
+
+        }
+
     }
 }//fecha contador
